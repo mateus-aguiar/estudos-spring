1af798b5342680f115ea979cd28472e9a25c9948
Aguiar <mateushenriqueaguiar@hotmail.com>
2025-04-30 17:58:55 -0300

    Dia 01 - Spring Boot Direto Das Trincheiras

---
+++ spring-boot-direto-das-trincheiras/anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
@@ -0,0 +1,20 @@
+package academy.devdojo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("v1/animes")
+@Slf4j
+public class AnimeController {
+
+    @GetMapping
+    public List<String> listAll() {
+        log.info(Thread.currentThread().getName());
+        return List.of("Naruto", "Boruto");
+    }
+}
